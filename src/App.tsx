40f79e9978ad3a662e71ba093c81dd82e66bdb4e
bruno lucas <bruno.lucas@hotmail.co.nz>
2020-11-09 23:13:17 +1300

    https://github.com/blucas2016

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//those components are required because I use it in dialog form
+import { Stack, PrimaryButton, TextField, DatePicker, initializeIcons } from 'office-ui-fabric-react';
+
+//initialization of icons - without calling this function icons for DatePicker would not be shown
+initializeIcons();
+declare const window: any;
+
+//this interface contains description of state and properties for Dialog application
+export interface IAKLDynamics365State {
+  //text input
+  text: string | undefined;
+  //date input
+  StartDate: Date | undefined;
+
+  EndDate: Date | undefined;
 }
 
-export default App;
+class AKLDynamics365 extends React.Component<IAKLDynamics365State, IAKLDynamics365State> {
+  constructor(props: IAKLDynamics365State) {
+    super(props);
+
+    //passing of data from properties to state of control during initialization
+    this.state = props;
+  }
+
+  //this function is used to control formatting of datetime field
+  private formatDate = (value?: Date | undefined): string => {
+    if (!value) {
+      return "";
+    }
+
+    let result = ("0" + (value.getMonth() + 1).toString()).slice(-2) + "/";
+    result += ("0" + value.getDate().toString()).slice(-2) + "/";
+    result += value.getFullYear().toString();
+
+    return result;
+  }
+
+  //heart of application that returns React markup
+  render() {
+    return (
+      <>
+        <TextField
+          label="Candidate name"
+          value={this.state.text}
+          onChange={(event: any, newvalue: string | undefined) => { this.setState({ text: newvalue }); }} />
+        <DatePicker
+          label="Interview Begin:"
+          value={this.state.StartDate}
+          onSelectDate={(newValue: Date | undefined | null) => { this.setState({ StartDate: newValue ? newValue : undefined }); }}
+          formatDate={this.formatDate}
+        />
+        <DatePicker
+          label="Interview End:"
+          value={this.state.EndDate}
+          onSelectDate={(newValue: Date | undefined | null) => { this.setState({ EndDate: newValue ? newValue : undefined }); }}
+          formatDate={this.formatDate}
+        />
+        <div className="footerDiv">
+          <Stack horizontal horizontalAlign={"end"} tokens={{ childrenGap: 10, padding: 10 }}>
+            <PrimaryButton text="OK" onClick={() => {
+              //This code on click of "OK" button returns current state to calling part
+              window.returnValue = this.state;
+             
+             
+              window.close();
+            }} />
+            <PrimaryButton text="Cancel" onClick={() => {
+              //This code closes the dialog window - ATM this is the only possible way
+              window.close();
+            }}/>
+          </Stack>
+        </div>
+      </>);
+  }
+}
+
+export default AKLDynamics365;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//following code parses url of dialog to extract required data including "data" parameter
+const queryString = window.location.search.substring(1);
+let params: any = {};
+const queryStringParts = queryString.split("&");
+for (let i = 0; i < queryStringParts.length; i++) {
+  const pieces = queryStringParts[i].split("=");
+  params[pieces[0].toLowerCase()] = pieces.length === 1 ? null : decodeURIComponent(pieces[1]);
+}
+
+//deserializing of the data parameter
+const data = JSON.parse(params.data);
+
+//rendering of application and passing parameters inside
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App text={data.text}  StartDate={new Date(data.date)} EndDate={new Date(data.date) } />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
